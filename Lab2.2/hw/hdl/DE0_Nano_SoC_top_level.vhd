d215756aec61699d333e657bf6ccbbeef5df20f6
maxdargent <maxime.dargent@epfl.ch>
2022-11-14 16:05:38 +0100

    DE0_Nano_SoC_top_level changed

--- Lab2.2/hw/hdl/DE0_Nano_SoC_top_level.vhd
+++ Lab2.2/hw/hdl/DE0_Nano_SoC_top_level.vhd
@@ -4,28 +4,28 @@ USE ieee.std_logic_1164.all;
 ENTITY DE0_Nano_SoC_top_level IS
 	PORT ( 	
 				GPIO_0           	: INOUT std_logic_vector(35 downto 0);
-				CLOCK_50 			: IN STD_LOGIC;
-				KEY 					: IN STD_LOGIC_VECTOR(0 DOWNTO 0)
+				FPGA_CLK1_50     : in    std_logic;
+				KEY_N            : in    std_logic_vector(1 downto 0)
 				
 			);
 END DE0_Nano_SoC_top_level;
 
 
 ARCHITECTURE Structure OF DE0_Nano_SoC_top_level IS
-
-	SIGNAL to_DAISYLED : STD_LOGIC_VECTOR(71 DOWNTO 0);
 	
 	COMPONENT system2 IS
 		PORT ( 	
-					clk_clk 				: IN STD_LOGIC;
-					to_led_readdata 	: OUT std_logic_vector(71 downto 0)         
+			reset_reset_n                    : in  std_logic                    := 'X'; -- reset_n
+			DaisyPort_LEDPort_export : out std_logic;        -- export
+			clk_clk                          : in  std_logic                    := 'X'  -- clk
 				);
 	END COMPONENT system2;
 
 BEGIN
 		U0: system2 PORT MAP (
-					clk_clk 				=> CLOCK_50,
-					to_led_readdata 	=> to_DAISYLED
+			reset_reset_n                    => KEY_N(0),
+			DaisyPort_LEDPort_export =>	GPIO_0(0),
+			clk_clk                          => FPGA_CLK1_50    
 				);
 		
 END Structure;
